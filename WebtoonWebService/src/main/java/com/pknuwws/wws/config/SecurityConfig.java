4c30d5edcc86c415ce33f27ee6c568c6f08f2233
pjm127 <mikeywc40@gmail.com>
2023-09-30 13:13:41 +0900

    "시큐리티 초기설정"

--- WebtoonWebService/src/main/java/com/pknuwws/wws/config/SecurityConfig.java
+++ WebtoonWebService/src/main/java/com/pknuwws/wws/config/SecurityConfig.java
@@ -1,4 +1,30 @@
 package com.pknuwws.wws.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+        ;
+        return http.build();
+    }
+
 }
