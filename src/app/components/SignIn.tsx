feec2d89693b02beebaac7038e5e3600d481b0bf
an-sangmoon <tkdans312@gmail.com>
2023-12-17 23:44:27 +0900

    signUp complete

--- src/app/components/SignIn.tsx
+++ src/app/components/SignIn.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { FormEvent } from 'react'
+import React, { MouseEvent, ChangeEvent, FormEvent, useState } from 'react'
 import CloseButton from './ui/CloseButton';
 
 type Props = {
@@ -7,26 +7,56 @@ type Props = {
 }
 
 export default function SignIn({onClose} : Props) {
+    const [loginInfo, setLoginInfo] = useState({
+        userId : "",
+        password : "",
+    })
+    const handleUserId = (e : ChangeEvent<HTMLInputElement>) => {
+        setLoginInfo((prevState)=>{
+            return {...prevState, userId : e.target.value};
+        })
+    }
+    const handlePassword = (e : ChangeEvent<HTMLInputElement>) => {
+        setLoginInfo((prevState)=>{
+            return {...prevState, password : e.target.value};
+        })
+    }
 
-    const onSubmit = (e : FormEvent) => {
-        e.preventDefault();
+    async function logIn(e : MouseEvent<HTMLInputElement>){
+        console.log("데이터 맞냐?", JSON.stringify(loginInfo));
+        (
+            async()=>{
+                await fetch('http://localhost:9001/api/v1/user/login',{
+                    method : 'POST',
+                    headers : {
+                        "Content-Type" : 'application/json',
+                    },
+                    body : JSON.stringify(loginInfo),
+                })
+                .then(res => console.log("응답은?? : ",res))
+            }
+        )()
     }
 
     return (
         <div className = 'fixed w-full h-full top-0 left-0 bg-neutral-900/60 z-50'>
-            <div className = 'fixed top-[20%] left-[35%] w-[500px] h-[300px] m-auto bg-white text-black rounded-xl p-5'>
-                <form onSubmit={onSubmit} className = 'p-5 flex flex-col items-center'>
+            <div className = 'absolute top-[20%] left-[35%] w-[500px] h-[300px] m-auto bg-white text-black rounded-xl p-5'>
+                <form name = 'login' className = 'p-5 flex flex-col items-center'>
                     <input 
                         type = 'text' 
+                        name = 'userId'
                         placeholder='아이디' 
                         className = 'p-2 shadow-sm mb-7'
+                        onChange={handleUserId}
                     ></input>
                     <input 
                         type = 'password' 
+                        name = 'password'
                         placeholder='비밀번호'
                         className= 'p-2 shadow-sm mb-10'
+                        onChange={handlePassword}
                     ></input>
-                    <input type='submit' className='p-3 w-20 text-white rounded-lg bg-sky-500'>로그인</input>
+                    <input type='button' className='p-3 w-20 text-white rounded-lg bg-sky-500' value = '로그인' onClick = {logIn}/>
                 </form>
             </div>
             <button onClick={onClose} className='fixed top-[10%] right-[15%]'>
--- src/app/components/SignUp.tsx
+++ src/app/components/SignUp.tsx
@@ -1,36 +1,164 @@
-import React from 'react'
+'use client'
+import { useRouter } from 'next/navigation';
+import React, { ButtonHTMLAttributes, ChangeEvent, useState, MouseEvent,FocusEvent, useRef } from 'react'
+
+const date = new Date()
+const year = date.getFullYear();
+const inputStyle = 'w-[70%] py-2 pl-3 rounded-md mb-5 text-black'
+const buttonStyle = 'w-[100px] p-2 mt-10 m-auto border-[0.05rem] border-sky-200 rounded-lg'
+const checkWarnStyle = 'text-red-700 bg-none pb-5 opacity-0 font-bold'
+const format = {
+    id : /^[a-z]+[a-z0-9]{5,16}$/,                                  // 아이디는 첫글자가 소문자 영어이며 소문자,영어를 섞은 5~16 글자
+    password : /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{6,16}$/,         // 비밀번호는 영어,숫자를 무조건 조합해야하며 소문자,대문자,숫자 조합의 6~16 글자
+    nickname : /^[가-힣a-zA-Z0-9]{2,12}$/,                           // 닉네임은 한글, 영어, 숫자를 조합한 2~12 글자
+}
 
 export default function SignUp() {
-    const date = new Date()
-    const year = date.getFullYear();
-    const inputStyle = 'w-[70%] py-2 pl-3 rounded-md mb-5 text-black'
-    const buttonStyle = 'w-[100px] p-2 mt-10 m-auto border-[0.05rem] border-sky-200 rounded-lg'
+
+    const router = useRouter();
+    const checkId = useRef<HTMLDivElement>(null);
+    const checkPwd = useRef<HTMLDivElement>(null);
+    const checkNickname = useRef<HTMLDivElement>(null);
+
+    const [userInfo, setUserInfo] = useState({
+        userId : "",
+        password : "",
+        nickName : "",
+        gender : "",
+        birthDate : "",
+    })
+    const [allCheck, setAllCheck] = useState<{[key : string]:boolean}>({
+        userId : false,
+        password : false,
+        nickName : false,
+    });
+    const [pwdCheck, setPwdCheck] = useState<string>('');
+
+    const handleUserId = (e:ChangeEvent<HTMLInputElement>) => {
+        setUserInfo((prevState)=>{
+            return {...prevState, userId : e.target.value}
+        })
+    }
+    const handlePassword = (e:ChangeEvent<HTMLInputElement>) => {
+        setUserInfo((prevState)=>{
+            return {...prevState, password : e.target.value}
+        })
+    }
+    const handleNickname = (e:ChangeEvent<HTMLInputElement>) => {
+        setUserInfo((prevState)=>{
+            return {...prevState, nickName : e.target.value}
+        })
+    }
+    const handleGender = (e:ChangeEvent<HTMLInputElement>) => {
+        setUserInfo((prevState)=>{
+            return {...prevState, gender : e.target.value}
+        })
+    }
+    const handleBirthDate = (e:ChangeEvent<HTMLInputElement>) => {
+        setUserInfo((prevState)=>{
+            return {...prevState, birthDate : e.target.value}
+        })
+    }
+
+    async function handleIdCheck(e:FocusEvent<HTMLInputElement>){
+        console.log("포커스 아웃!!!!!!!!")
+        const res = await fetch(`http://localhost:9001/api/v1/user/check-id?userId=${userInfo.userId}`)
+        if(res.ok === true && format.id.test(userInfo.userId)){
+            (checkId.current as HTMLDivElement).style.setProperty('opacity','0')
+            setAllCheck((prevState)=>{ return {...prevState, userId : true}})
+        }
+        else {
+            (checkId.current as HTMLDivElement).style.setProperty('opacity','1')
+            setAllCheck((prevState)=>{ return {...prevState, userId : false}})
+        }
+    }
+    async function handleNickNameCheck(){
+        const res = await fetch(`http://localhost:9001/api/v1/user/check-nickname?nickName=${userInfo.nickName}`)
+        if(res.ok === true && format.nickname.test(userInfo.nickName)){
+            (checkNickname.current as HTMLDivElement).style.setProperty('opacity','0')
+            setAllCheck((prevState)=>{ return {...prevState, nickName : true}})
+        }
+        else {
+            (checkNickname.current as HTMLDivElement).style.setProperty('opacity','1')
+            setAllCheck((prevState)=>{ return {...prevState, nickName : false}})
+        }
+    }
+    function handlePwdCheck(e:FocusEvent<HTMLInputElement>){
+        if(pwdCheck === userInfo.password && format.password.test(userInfo.password)){
+            (checkPwd.current as HTMLDivElement).style.setProperty('opacity', '0')
+            setAllCheck((prevState)=>{ return {...prevState, password : true}})
+        }
+        else {
+            (checkPwd.current as HTMLDivElement).style.setProperty('opacity', '1')
+            setAllCheck((prevState)=>{ return {...prevState, password : false}})
+        }
+    }
+    function infoAllCheck() : boolean{
+        let check = false;
+        for(const key in allCheck){
+            if(allCheck[key] === false){
+                check = false;
+                break;
+            } 
+            check = true;
+        }
+        return check;
+    } 
+
+    const infoSubmit = (e :  MouseEvent<HTMLInputElement>) => {
+        console.log(" 객체 @@@@", JSON.stringify(userInfo));
+        if(infoAllCheck()){
+            (
+                async()=>{
+                    await fetch('http://localhost:9001/api/v1/user/join',{
+                        method : 'POST',
+                        headers : {
+                            "Content-Type" : 'application/json',
+                        },
+                        body : JSON.stringify(userInfo),
+                    })
+                    // .then(res => console.log("응답번호 : ",res))
+                }
+            )()
+            router.push('/');
+        }
+        else window.alert("다시 입력하세요.");
+    }
+
 
     return (
-        <div className = 'p-5 mt-16 w-[80%] md:w-[600px] m-auto text-white transition-all duration-500'>
-            <form className = 'flex flex-col' action='http://localhost:9001/api/v1/user/join' method='post' name = 'signup' encType='application/json'>
+        <div className = 'p-5 mt-16 w-[450px] md:w-[600px] m-auto text-white transition-all duration-500'>
+            <form className = 'flex flex-col' name = 'signup'>
                     <label>
-                        <input type = 'text' name = 'userId' placeholder='아이디' className = {`${inputStyle}`} required/>
-                        <button className = {`${buttonStyle} ml-4`}>중복 확인</button>
+                        <input type = 'text' name = 'userId' placeholder='아이디' className = {`${inputStyle}`} maxLength={16} required onChange={handleUserId} onBlur={handleIdCheck}/>
+                        <div ref={checkId} className = {checkWarnStyle}> 사용할 수 없는 아이디입니다. 다시 입력해주세요.</div>
                     </label>
-                    <input type = 'password' name = 'password' placeholder='비밀번호' className = {`${inputStyle}`} required/>
+                    <input type = 'password' name = 'password' placeholder='비밀번호' className = {`${inputStyle}`} maxLength={20} required  onChange={handlePassword} onBlur={ handlePwdCheck}/>
+                    <input type = 'password' placeholder='비밀번호 확인' className = {`${inputStyle}`} maxLength={20} required onChange={e=>setPwdCheck(e.target.value)} onBlur={handlePwdCheck}/>
+                    <div ref={checkPwd} className={checkWarnStyle}>비밀번호가 일치하지 않습니다. 다시 입력해주세요.</div>
                     <label>
-                        <input type = 'password'  placeholder='비밀번호 확인' className = {`${inputStyle}`}/>
-                        <button className = {`${buttonStyle} ml-4`}>중복 확인</button>
+                        <input type = 'text' name = 'nickName' placeholder='닉네임' className = {`${inputStyle}`} maxLength={12} required  onChange={handleNickname} onBlur={handleNickNameCheck}/>
+                        <div ref={checkNickname} className={checkWarnStyle}>사용할 수 없는 닉네임입니다. 다시 입력해주세요.</div>
                     </label>
-                    <input type = 'text' name = 'nickName' placeholder='닉네임' className = {`${inputStyle}`} required/>
                     <label className = 'mb-5'>
-                        <input type = 'radio' name = 'gender' className = 'mr-3' value = 'MALE' /><label>남</label>
-                       <input type = 'radio' name = 'gender' className = 'mr-3 ml-3' value = 'FEMALE' /> <label>여</label>
+                        <input type = 'radio' name = 'gender' className = 'mr-3' value = 'MALE' onChange={handleGender} required/><label>남</label>
+                       <input type = 'radio' name = 'gender' className = 'mr-3 ml-3' value = 'FEMALE' onChange={handleGender} required/> <label>여</label>
                     </label>
                     <label>
                         <label className = 'mr-3'>
                             생년월일
                             
                         </label>
-                        <input type = 'date' name = 'birthDate' min={'1900-01-01'} max={`${year}-12-31`} className = 'p-1 ml-4 text-black rounded-md' required/>
+                        <input 
+                            type = 'date' 
+                            name = 'birthDate' 
+                            min={'1900-01-01'} 
+                            max={`${year}-12-31`} 
+                            className = 'p-1 ml-4 text-black rounded-md' 
+                            required 
+                            onChange={handleBirthDate}/>
                     </label>
-                    <input type = 'submit' className = {`${buttonStyle}`} value="가입"/>
+                    <input type = 'button' className = {`${buttonStyle}`} value="가입"  onClick = {infoSubmit}/>
             </form>
         </div>
     )
